994971a86a57a754df67a446e992472f938b4e66
remmo <remigiusz.pisarski@onet.pl>
2022-02-02 20:28:31 +0100

    I learned about collections of components.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,9 +2,20 @@ import React, { Component } from 'react';
 
 class Counter extends Component { 
     state = {
-        count: 0,
-        imageUrl: "https://picsum.photos/200",
-        tags : ['tag1', 'tag2', 'tag3']
+        value: this.props.value
+
+        /* dodatki niepotrzebne do dzialania aplikacji
+        
+            imageUrl: "https://picsum.photos/200",
+             <img src={this.state.imageUrl} alt=""></img>
+
+
+            tags : ['tag1', 'tag2', 'tag3']
+            <div>
+                {this.state.tags.length === 0 && 'please create a new tag!'}
+                {this.renderTags()}
+            </div>
+        */
     };
 
     styles = {
@@ -23,7 +34,7 @@ class Counter extends Component {
 
     handleIncrement = product => {
         console.log(product);
-        this.setState({ count: this.state.count + 1 });
+        this.setState({ value: this.state.value + 1 });
     };
 
     renderTags() {
@@ -32,36 +43,27 @@ class Counter extends Component {
         return <ul> {this.state.tags.map(tag => <li key={tag}>{tag}</li>)} </ul>
     };
 
-    render() { 
-
+    render() {
         return ( 
         <React.Fragment>
-            <img src={this.state.imageUrl} alt=""></img>
-
             <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
 
             <button 
                 className="btn btn-secondary btn-sm" 
                 onClick={ () => this.handleIncrement({id : 1}) }>Increment
             </button>
-
-            <div>
-                {this.state.tags.length === 0 && 'please create a new tag!'}
-                {this.renderTags()}
-            </div>
-
         </React.Fragment>
             );
     }
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (this.state.value === 0) ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const {count} = this.state;
+        const {value: count} = this.state;
         return count === 0 ? "Zero" : count;
     }
 }
